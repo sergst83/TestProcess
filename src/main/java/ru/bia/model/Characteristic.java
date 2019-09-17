d5d53ebc1cbcf6232564340251d18faed60054ed
wbadmin <>
2019-09-17 10:38:30 +0000

    {/src/main/java/ru/bia/model/Characteristic.java}

--- src/main/java/ru/bia/model/Characteristic.java
+++ src/main/java/ru/bia/model/Characteristic.java
@@ -6,12 +6,47 @@ package ru.bia.model;
 
 public class Characteristic implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Characteristic() {
-    }
+	@org.kie.api.definition.type.Label(value = "Адрес отправителя")
+	private java.lang.String addressFrom;
+	@org.kie.api.definition.type.Label(value = "Адрес получателя")
+	private java.lang.String addressTo;
+	@org.kie.api.definition.type.Label(value = "Груз")
+	private ru.bia.model.Cargo cargo;
 
+	public Characteristic() {
+	}
 
+	public java.lang.String getAddressFrom() {
+		return this.addressFrom;
+	}
 
+	public void setAddressFrom(java.lang.String addressFrom) {
+		this.addressFrom = addressFrom;
+	}
+
+	public java.lang.String getAddressTo() {
+		return this.addressTo;
+	}
+
+	public void setAddressTo(java.lang.String addressTo) {
+		this.addressTo = addressTo;
+	}
+
+	public ru.bia.model.Cargo getCargo() {
+		return this.cargo;
+	}
+
+	public void setCargo(ru.bia.model.Cargo cargo) {
+		this.cargo = cargo;
+	}
+
+	public Characteristic(java.lang.String addressFrom,
+			java.lang.String addressTo, ru.bia.model.Cargo cargo) {
+		this.addressFrom = addressFrom;
+		this.addressTo = addressTo;
+		this.cargo = cargo;
+	}
 
 }
